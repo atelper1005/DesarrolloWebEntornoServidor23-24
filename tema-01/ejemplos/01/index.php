fc8b14acd38b44679d80ecfd7fdb2a670a001c23
unknown <you@example.com>
2023-09-20 14:36:49 +0200

    first commit

---
+++ tema-01/ejemplos/01/index.php
@@ -0,0 +1,19 @@
+<?php
+
+    /*
+        
+        Ejemplo 1 - PHP
+        Hola Mundo
+
+    */
+
+    echo '<h1>';
+    echo '<center>';
+    echo 'Hola Mundo PHP';
+    echo '<BR>';
+    echo 'Soy Antonio Jesús Téllez Perdigones';
+    echo '<hr>';
+    echo '</h1>';
+    echo '2ºDAW - DWES - Curso 23/24';
+
+?>

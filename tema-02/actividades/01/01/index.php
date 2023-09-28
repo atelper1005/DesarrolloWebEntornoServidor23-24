94c7d09ec5e75e63ae169ce030be111523c9adbb
atelper1005 <116277422+atelper1005@users.noreply.github.com>
2023-09-28 17:19:39 +0200

    Update index.php

--- tema-02/actividades/01/01/index.php
+++ tema-02/actividades/01/01/index.php
@@ -9,20 +9,16 @@
 
 <body>
     <center>
-        <h2>La Inflación</h2>
+        <h1><?php La Inflación ?></h1>
     </center>
 
-        <h4>
+        <p>
     <?php
     echo "La subida de los precios de la energía hizo más empinada la cuesta de septiembre que los hogares encararon tras unas vacaciones especialmente caras.";
-    echo "<BR>";
-    echo "La inflación trepó este mes hasta el 3,5%, según ha avanzado este jueves el Instituto Nacional de Estadística (INE).";
-    echo "<BR>";
-    echo "El organismo atribuye ese dato, nueve décimas superior al registrado en agosto, sobre todo a la subida de los precios de la electricidad.";
         ?>
-        </h4>
+        </p>
 
-        
+        <a href="http://www.elpais.es" target>
 
 </body>
 

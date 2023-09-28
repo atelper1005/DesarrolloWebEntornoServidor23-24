7ccbad6ee662ad6024f73a23646cd1359a86a8f8
atelper1005 <116277422+atelper1005@users.noreply.github.com>
2023-09-28 17:29:43 +0200

    Update index.php

--- tema-02/actividades/01/03/index.php
+++ tema-02/actividades/01/03/index.php
@@ -1,1 +1,11 @@
-<?php ?>
+<?php
+// Crear dos variables de tipo string
+$mensaje1 = "Hola, ";
+$mensaje2 = "mundo!";
+
+// Concatenar las dos variables y guardar el resultado en una nueva variable
+$resultado = $mensaje1 . $mensaje2;
+
+// Mostrar el resultado
+echo $resultado;
+?>

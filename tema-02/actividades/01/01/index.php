ac0b49f18d3ecb11bb201c909955a7ae8f7a442b
unknown <you@example.com>
2023-09-28 10:30:38 +0200

    ejercicios tema 2

--- tema-02/actividades/01/01/index.php
+++ tema-02/actividades/01/01/index.php
@@ -1,1 +1,29 @@
-<?php ?>
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Actividad 2.1</title>
+</head>
+
+<body>
+    <center>
+        <h2>La Inflación</h2>
+    </center>
+
+        <h4>
+    <?php
+    echo "La subida de los precios de la energía hizo más empinada la cuesta de septiembre que los hogares encararon tras unas vacaciones especialmente caras.";
+    echo "<BR>";
+    echo "La inflación trepó este mes hasta el 3,5%, según ha avanzado este jueves el Instituto Nacional de Estadística (INE).";
+    echo "<BR>";
+    echo "El organismo atribuye ese dato, nueve décimas superior al registrado en agosto, sobre todo a la subida de los precios de la electricidad.";
+        ?>
+        </h4>
+
+        
+
+</body>
+
+</html>

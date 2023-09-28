d7612dcb06548c8ef565f58430f1b7927c98536c
atelper1005 <116277422+atelper1005@users.noreply.github.com>
2023-09-28 17:38:05 +0200

    Update index.php

--- tema-02/actividades/01/05/index.php
+++ tema-02/actividades/01/05/index.php
@@ -1,1 +1,25 @@
-<?php ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Información de PHP</title>
+    <style>
+        body {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            margin: 0;
+        }
+        #phpinfo {
+            text-align: left;
+        }
+    </style>
+</head>
+<body>
+    <div id="phpinfo">
+        <?php
+        phpinfo();
+        ?>
+    </div>
+</body>
+</html>

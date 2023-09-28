398507c9569583f870bcb879bf9d7ec1b88edb33
atelper1005 <116277422+atelper1005@users.noreply.github.com>
2023-09-28 17:27:27 +0200

    Update index.php

--- tema-02/actividades/01/02/index.php
+++ tema-02/actividades/01/02/index.php
@@ -1,1 +1,27 @@
-<?php ?>
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Actividad 2.1</title>
+</head>
+
+<body>
+    <center>
+        <h1><?php La Inflación ?></h1>
+    </center>
+
+        <p>
+    <?php
+    echo "La subida de los precios de la energía hizo más empinada la cuesta de septiembre que los hogares encararon tras unas vacaciones especialmente caras.";
+        ?>
+        </p>
+
+        <a href="http://www.elpais.es" target>
+
+        <img src="" alt="" width="300">
+
+</body>
+
+</html>

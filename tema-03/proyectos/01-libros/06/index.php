c044a93ac5eae6626cb9e105392456d71c68e3b1
atelper1005 <116277422+atelper1005@users.noreply.github.com>
2023-10-23 14:25:14 +0200

    06
    git bash no funca

---
+++ tema-03/proyectos/01-libros/06/index.php
@@ -0,0 +1,20 @@
+<?php
+
+    /*
+    
+        Controlador: index.php
+
+        
+    */
+
+    #Libreria
+    include 'libs/crud_funciones.php';
+
+    #Modelo
+    include 'models/modelIndex.php';
+
+    #Vista
+    //Cargo la vista
+    include "views/viewIndex.php";
+    
+?>
---
+++ tema-03/proyectos/01-libros/06/models/model.eliminar.php
@@ -0,0 +1,28 @@
+<?php
+    /*
+    
+        Modelo: model.eliminar.php
+        Descripción: elimina un elemento de la tabla
+
+        Método GET:
+                    -id del libro que quiero eliminar
+    
+    */
+
+    $id = $_GET['id'];
+
+    $indice_eliminar = buscar_en_tabla($libros, 'id', $id);
+
+    //comparacion estricta para distinguir el false del 0
+    if ($indice_eliminar !== false) {
+        //elimino el libro seleccionado
+        unset($libros[$indice_eliminar]);
+
+        //reconstruyo el array
+        $libros = array_values($libros);
+    } else {
+        echo 'ERROR: libro no encontrado';
+        exit();
+    }
+
+?>

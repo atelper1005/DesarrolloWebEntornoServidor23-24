b4d44e578293ac2d739b73a3b45b063f5ae3740a
atelper1005 <atelper1005@g.educaand.es>
2023-11-21 10:50:24 +0100

    Mejora examen tema4

--- tema-04/examenes/01-jugadores-ss/views/view.mostrar.php
+++ tema-04/examenes/01-jugadores-ss/views/view.mostrar.php
@@ -16,40 +16,40 @@
         <!-- id -->
         <div class="mb-3">
             <label for="id" class="form-label">Id</label>
-            <input type="text" class="form-control" name="id" value="<?= $jugador['id']?>" readonly>
+            <input type="text" class="form-control" name="id" value="<?= $jugador->getId() ?>" readonly>
             <!-- <div class="form-text">Introduzca identificador del libro</div> -->
         </div>
         <!-- Descripción -->
         <div class="mb-3">
             <label for="nombre" class="form-label">Nombre</label>
-            <input type="text" class="form-control" name="nombre" value="<?= $jugador['nombre']?>" readonly >
+            <input type="text" class="form-control" name="nombre" value="<?= $jugador->getNombre() ?>" readonly >
         </div>
         <!-- Modelo -->
         <div class="mb-3">
             <label for="numero" class="form-label">Num</label>
-            <input type="text" class="form-control" name="numero" value="<?= $jugador['numero']?>" readonly>
+            <input type="text" class="form-control" name="numero" value="<?= $jugador->getNumero() ?>" readonly>
             <!-- <div class="form-text">Introduzca Autor del libro</div> -->
         </div>
         <!-- Categoría Select -->
         <div class="mb-3">
             <label for="pais" class="form-label">País</label>
-            <input type="text" class="form-control" name="pais" value="<?= $paises[$jugador['pais']]?>" readonly>
+            <input type="text" class="form-control" name="pais" value="<?= $jugador->getPais() ?>" readonly>
         </div>
         <!-- Unidades -->
         <div class="mb-3">
             <label for="equipo" class="form-label">Equipo</label>
-            <input type="text" class="form-control" name="equipo" step="0.01" value="<?= $jugador['equipo']?>" readonly >
+            <input type="text" class="form-control" name="equipo" step="0.01" value="<?= $jugador->getEquipo() ?>" readonly >
             <!-- <div class="form-text">Introduzca Precio</div> -->
         </div>
         <div class="mb-3">
             <label for="posiciones" class="form-label">Posiciones</label>
-            <input type="text" class="form-control" name="posiciones" step="0.01" value="<?= $jugador['posiciones']?>" readonly >
+            <input type="text" class="form-control" name="posiciones" step="0.01" value="<?= $jugador->getPosicion() ?>" readonly >
             <!-- <div class="form-text">Introduzca Precio</div> -->
         </div>
         <!-- Precio -->
         <div class="mb-3">
             <label for="contrato" class="form-label">Contrato (€)</label>
-            <input type="number" class="form-control" name="contrato" step="0.01" value="<?= $jugador['contrato']?>" readonly >
+            <input type="number" class="form-control" name="contrato" step="0.01" value="<?= $jugador->getContrato() ?>" readonly >
             <!-- <div class="form-text">Introduzca Precio</div> -->
         </div>
         

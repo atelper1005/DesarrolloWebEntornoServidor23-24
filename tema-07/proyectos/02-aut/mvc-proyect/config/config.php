a0dbc5dbfe9fb4f29b7716243962c59c691deafc
atelper1005 <atelper1005@g.educaand.es>
2024-01-22 10:22:20 +0100

    Proyectos 7.1 y 7.2

---
+++ tema-07/proyectos/02-aut/mvc-proyect/config/config.php
@@ -0,0 +1,15 @@
+<?php
+# Configuración básica aplicación MVC
+
+# Ruta absoluta
+define('URL', 'http://localhost/DWES/tema-07/proyectos/02-aut/mvc-proyect/');
+
+# Constante de la Base de Datos
+define('HOST', 'localhost');
+define('DB', 'gesbank');
+define('USER', 'root');
+define('PASSWORD', '');
+define('CHARSET', 'utf8mb4');
+
+
+?>
